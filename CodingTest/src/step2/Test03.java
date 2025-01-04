6990d512834cca0225faedbce6a74f6c325b4ad5
진우 <128763219+Jinwoooooooo@users.noreply.github.com>
2025-01-04 15:27:50 +0900

    Clear Test03

--- CodingTest/src/step2/Test03.java
+++ CodingTest/src/step2/Test03.java
@@ -1,5 +1,17 @@
 package step2;
 
-public class Test03 {
+import java.util.Scanner;
 
+public class Test03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int year = sc.nextInt();
+		
+		if(year%4==0 && (year%100!=0 || year%400==0)) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+	}
 }
