dadc9fdc679ddebab5878704a0dd1f0b7aefd842
jinwoooooooo <jinwooya55@gmail.com>
2025-01-06 17:40:18 +0900

    Clear Test06

--- CodingTest/src/step2/Test06.java
+++ CodingTest/src/step2/Test06.java
@@ -3,21 +3,23 @@ package step2;
 import java.util.Scanner;
 
 public class Test06 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int h = sc.nextInt();
-		int m = sc.nextInt();
-		int add = sc.nextInt();
-		
-		if(h == 0) {
-			h = 24;
-			if(m >= 60) {
-				h += 1;
-				m = m + (m-60);
-			} else {
-				
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int time = sc.nextInt();
+
+        h += time/60; //시간 = 시간 + 타이머시간과 60을 나눈 몫
+        m += time%60; // 분 = 분 + 타이머시간과 60을 나눈 나머지
+
+        if (m >= 60) {
+            h += 1;
+            m -= 60;
+        }
+
+        if (h >= 24) {
+            h %= 24;
+        }
+        System.out.print(h + " " + m);
+    }
 }
