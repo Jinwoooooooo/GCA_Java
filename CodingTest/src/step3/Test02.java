44bc1160c8b4a57ff0dd8c657ea7a4af1244317d
jinwoooooooo <jinwooya55@gmail.com>
2025-01-08 15:20:06 +0900

    Clear Test02

--- CodingTest/src/step3/Test02.java
+++ CodingTest/src/step3/Test02.java
@@ -6,9 +6,17 @@ public class Test02 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+		int time = sc.nextInt();
+		int[] sum = new int[time];
 		
-		System.out.println(a + b);
+		for(int i=0; i<time; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			sum[i] = a + b;
+		}
+		
+		for(int i=0; i<sum.length; i++) {			
+			System.out.println(sum[i]);
+		}
 	}
 }
